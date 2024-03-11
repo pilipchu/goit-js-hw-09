f7c98265948bada555ac5478275f2775f4d066f3
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-03-11 13:33:49 +0200

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,32 +1,42 @@
 const nameKey = "feedback-form-state"
 const form = document.querySelector('.feedback-form')
-const textEmail = document.querySelector('input')
-const textInput = {
+const textForm = {
     email: '',
-    message: ''
+    message:''
 }
 
-textEmail.addEventListener('input', (event) =>
-    textInput.email = event.target.value
-)
+const getMessage = () => {
+        textForm.email=form.elements.email.value.trim(),
+        textForm.message=form.elements.message.value.trim(),
+    localStorage.setItem(nameKey, JSON.stringify(textForm));
+}
 
-
+const load = () => {
+    const data = localStorage.getItem(nameKey)
+    if (data) {
+        const { email, message } = JSON.parse(data)
+        form.elements.email.value = email
+        form.elements.message.value = message
+    }
+}
+ 
+load()
 
 function formSubmit(event) {
     event.preventDefault()
-    if (textInput.message === '') {
-        alert("Введіть ваше повідомлення")
-    }
-    if (textInput === '') {
-        alert('Введіть ваш email')
-    }
-    const deta = JSON.stringify(textInput)
-        localStorage.setItem(nameKey, deta)
-    console.log(JSON.parse(deta))
+    if (form.elements.message.value !== '' && form.elements.email.value !== '') {
+        console.log(textForm)
     form.reset()
-        localStorage.removeItem(nameKey)
+    localStorage.removeItem(nameKey)
+    } else {
+        alert ("Не всі поля заповнені")
+    }
 }
 
+form.addEventListener('input', getMessage) 
+
 form.addEventListener('submit', formSubmit)
+
+
 
 
